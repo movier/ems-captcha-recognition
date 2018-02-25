a20dc202334ff97411e17e5aad5d35bbc71d15cc
Oliver <olimovier@gmail.com>
2018-02-26 00:27:04 +0800

    Put segment number into corresponding directory

--- captcha.py
+++ captcha.py
@@ -1,4 +1,11 @@
 from PIL import Image
+import random
+import os
+
+for i in range(10):
+    dir = "./%d" % i
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
 name = "016586.jpeg"
 value = name.split(".")[0]
@@ -10,4 +17,5 @@ h = 20
 for i in range(6):
     x += w if i > 0 else 0 
     region = im.crop((x, y, x + w, y + h))
-    region.save("%s.jpeg"%value[i])
+    name = random.randint(1, 9999)
+    region.save("./%s/%d.jpeg" % (value[i], name))
