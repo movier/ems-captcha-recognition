73cd55952f42cfd47b8d6d786f126cfdb236293b
Oliver <olimovier@gmail.com>
2018-02-26 11:48:26 +0800

    Create a direcory to contain all image categories

--- captcha.py
+++ captcha.py
@@ -2,8 +2,12 @@ from PIL import Image
 import random
 import os
 
+imageDir = "./digit_images"
+if not os.path.exists(imageDir):
+    os.makedirs(imageDir)
+
 for i in range(10):
-    dir = "./%d" % i
+    dir = "./%s/%d" % (imageDir, i)
     if not os.path.exists(dir):
         os.makedirs(dir)
 
@@ -20,4 +24,4 @@ for name in os.listdir(dataDir):
         x += w if i > 0 else 0 
         region = im.crop((x, y, x + w, y + h))
         segmentName = random.randint(1, 9999)
-        region.save("./%s/%d.jpeg" % (value[i], segmentName))
+        region.save("./%s/%s/%d.jpeg" % (imageDir, value[i], segmentName))
