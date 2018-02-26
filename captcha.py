16d6e1ba483cbccd6858e92957d7558444f19cb4
Oliver <olimovier@gmail.com>
2018-02-26 11:37:34 +0800

    Foreach all raw captcha images do preprocessing

--- captcha.py
+++ captcha.py
@@ -7,15 +7,17 @@ for i in range(10):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-name = "016586.jpeg"
-value = name.split(".")[0]
-im = Image.open(name)
-x = 5
+dataDir = "./captcha/"
 y = 0
 w = 13
 h = 20
-for i in range(6):
-    x += w if i > 0 else 0 
-    region = im.crop((x, y, x + w, y + h))
-    name = random.randint(1, 9999)
-    region.save("./%s/%d.jpeg" % (value[i], name))
+
+for name in os.listdir(dataDir):
+    x = 5
+    value = name.split(".")[0]
+    im = Image.open(dataDir + name)
+    for i in range(6):
+        x += w if i > 0 else 0 
+        region = im.crop((x, y, x + w, y + h))
+        segmentName = random.randint(1, 9999)
+        region.save("./%s/%d.jpeg" % (value[i], segmentName))
